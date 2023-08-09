c4f9c329fe7f3a5766c43aa9fd4bc8f56ec60645
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2023-08-09 17:49:40 +0400

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
